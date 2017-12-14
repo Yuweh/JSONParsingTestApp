65a7be0fe1309d4b7da203945bebed27fdd1cbdb
Francis Jemuel Bergonia <francisb.itcareers@gmail.com>
2017-12-14 09:10:35 +0800

    App Complete

--- JSONParsingTestApp/ViewController.swift
+++ JSONParsingTestApp/ViewController.swift
@@ -8,18 +8,67 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
+    //MARK: Properties
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var tableData:[[String: AnyObject]] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
+        // 1. Read JSON from bundle and convert it to data
+        let url = Bundle.main.url(forResource: "CelestialBeing", withExtension: "json")
+        
+        if let url = url{
+            let data = NSData(contentsOf: url)
+            
+            // 2. serialize JSON from Data
+            if let data = data {
+                // 1. print(data)
+                do {
+                    let jsonObject = try JSONSerialization.jsonObject(with: data as Data, options: .allowFragments)
+                   
+                    // 2. print(jsonObject)
+                    
+                    // 3. get or read data from Dictionary from jsonObject
+                    if let object = jsonObject as? [String: AnyObject] {
+                        if let allPersonnel = object["CelestialBeing"] as? [[String:AnyObject]] {
+                            
+                            // 3. print(allPersonnel)
+                            tableData = allPersonnel
+                            tableView.reloadData()
+                        }
+                    }
+                    
+                } catch {
+                    print("error")
+                }
+            }
+        }
+    }
+    
+    // 4. Displaying JSOn content on tableview
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tableData.count
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "jsonCell", for: indexPath)
+        
+        // 4. setting JSON data on the tableview cell
+        
+        let CelestialBein = tableData[indexPath.row]
+        let name = CelestialBein["fullName"] as? String
+        let ms = CelestialBein["ms"] as? String
+        
+        cell.textLabel?.text = name
+        cell.detailTextLabel?.text = ms
+        
+        return cell
     }
-
-
 }
 
